4facc6019924e393249c8bc2de497fd91639353f
Aleh <oleglevshunov1988@gmail.com>
2025-02-12 21:54:25 +0300

    Feature add new socket log watcher

---
+++ src/test/java/com/eurotorg/test_logs/TestLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurotorg.test_logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
